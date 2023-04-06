fdb2f000cc60f809912a4aa9e0c32c32958ddb88
Vitor Alves <vitor.de.souza.948@gmail.com>
2023-04-06 18:52:03 -0300

    Delete scripts directory

--- scripts/toggle-touchpad.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ $(gsettings get org.gnome.desktop.peripherals.touchpad send-events) == "'enabled'" ]; then
-    echo "Switching off"
-    gsettings set org.gnome.desktop.peripherals.touchpad send-events disabled
-else
-    echo "Switching on"
-    gsettings set org.gnome.desktop.peripherals.touchpad send-events enabled
-fi
