9e9821de7da94d6e4f02842356540766974bbd61
Vitor Alves <vitor.de.souza.948@gmail.com>
2023-04-06 18:51:51 -0300

    Delete firefox-css directory

--- firefox-css/change-firefox.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CHROME="$HOME/.mozilla/firefox/ouga74te.default-esr/chrome"
-CHROME_COPY="$HOME/projetos/firefox-css"
-
-rm $CHROME/userChrome.css
-cp $CHROME_COPY/userChrome.css $CHROME/
